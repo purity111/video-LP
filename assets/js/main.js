132e50fef3a54a2c30ed46ec989a925795787ac4
strong <strong.business.info@gmail.com>
2024-11-22 01:39:39 +0900

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const FV = document.getElementById('FV');
+const main_video = document.getElementById('main-video');
+const detailBtn = document.getElementById('detailBtn');
+const topBtn = document.getElementById('topBtn');
+
+detailBtn.addEventListener('click', () => {
+    FV.style.display = 'none';
+    FV.pause();
+    main_video.style.display = 'block';
+    topBtn.style.display = 'block';
+    this.style.display = 'none';
+})
+
+topBtn.addEventListener('click', () => {
+    window.location.reload();
+})
